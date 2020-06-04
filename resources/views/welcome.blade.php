7a67a288aee6ae0ae2c38941ea34eb2230fbbd71
Tri Nhan Vu <trinhanvu@gmail.com>
2020-06-04 14:14:09 +0200

    updated welcome page with basic information and links to pb and chainist

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,17 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PositiveBlockchain API & Web App
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://positiveblockchain.io">PositiveBlockchain.io</a>
+                    <a href="https://github.com/PositiveBlockchain/pb-web-app">Github</a>
+                    <a href="https://positiveblockchain.io/about">About PB</a>
+                    <a href="https://twitter.com/PositiveBlock">Twitter</a>
+                    <a href="https://www.youtube.com/channel/UCfUUntxXsz0k1N1oCb7w1ZA">YouTube</a>
+                    <a href="https://www.linkedin.com/company/positiveblockchain-io/">LinkedIn</a>
+                    <a href="https://chainist.de">Hosted and Co-Developed by Chainist.de</a>
                 </div>
             </div>
         </div>
