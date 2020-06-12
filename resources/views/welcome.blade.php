d1661100ce9517691c9dbbfeff107bb70e8b2002
Tri Nhan Vu <trinhanvu@gmail.com>
2020-06-12 15:56:36 +0200

    add note for development

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,6 +44,7 @@
                 <h1 class="text-gray-600 text-center font-light tracking-wider text-5xl mb-6">
                     Blockchain Social Impact <br> Projects   Dashboard
                 </h1>
+                <p class="text-center mb-5"><small>This is a development version. Things might break.</small></p>
                 <App></App>
                 <footer class="mt-5">
                     <ul class="list-reset">
