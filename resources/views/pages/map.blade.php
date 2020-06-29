f03849c09d2ff7c46ca5d4e50ba76fbe7b97cbe1
Tri Nhan Vu <trinhanvu@gmail.com>
2020-06-29 22:04:07 +0200

    add headline and short description for the map

--- resources/views/pages/map.blade.php
+++ resources/views/pages/map.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.app')
 @section('content')
     <div class="container mx-auto">
+        <h1 class="text-center font-bold text-2xl mb-2">Global Social Impact Map</h1>
+        <p class="text-center mb-5">The map loads currently 500 projects from the database. <br>The performance is a bit
+            slow, as the markers
+            take a bit longer to be rendered. <br> Click on a marker to get more details about the project.</p>
+
         <div class="flex items-center">
+
             <div class="md:w-full md:mx-auto">
                 <project-map>
 
