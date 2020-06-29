81dac0cef6fc6f9d4589b5caf2d4fbec539bebb2
Tri Nhan Vu <trinhanvu@gmail.com>
2020-06-29 22:12:26 +0200

    rephrase the text

--- resources/views/pages/map.blade.php
+++ resources/views/pages/map.blade.php
@@ -2,9 +2,7 @@
 @section('content')
     <div class="container mx-auto">
         <h1 class="text-center font-bold text-2xl mb-2">Global Social Impact Map</h1>
-        <p class="text-center mb-5">The map loads currently 500 projects from the database. <br>The performance is a bit
-            slow, as the markers
-            take a bit longer to be rendered. <br> Click on a marker to get more details about the project.</p>
+        <p class="text-center mb-5"><br> Click on a marker to get more details about the project.</p>
 
         <div class="flex items-center">
 
@@ -14,6 +12,8 @@
                 </project-map>
             </div>
         </div>
-        <p class="text-sm mt-2">The map tiles are provided by <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>.</p>
+        <p class="text-sm mt-2">The map tiles are provided by <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>.
+            The performance can a bit slow, as the markers take a bit longer to be rendered. In another version markers
+            will be clustered.</p>
     </div>
 @endsection
